698bad8e5cd6a952d664f27f942fc27ca21c1531
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-02-26 12:47:07 -0800

    Rework to look for long queue times

--- src/main/java/com/github/trask/GitHubActionsAnalyzer.java
+++ src/main/java/com/github/trask/GitHubActionsAnalyzer.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -15,8 +15,14 @@ import java.util.stream.Collectors;
 
 public class GitHubActionsAnalyzer {
 
-  private static final LocalDate START_DATE = LocalDate.of(2023, 1, 23);
-  private static final ZoneId TIME_ZONE = ZoneId.of("America/Los_Angeles");
+  private static final Instant START = Instant.parse("2023-01-01T00:00:00Z");
+  private static final Instant END = Instant.parse("2023-03-01T00:00:00Z");
+
+  private static final int PAGE_SIZE = 100; // this is the largest possible page size
+
+  private static final Duration MIN_QUEUE_TIME = Duration.ofMinutes(30);
+
+  private static final Duration WORKFLOW_TIMEOUT = Duration.ofHours(6);
 
   private static final ExecutorService executor = Executors.newFixedThreadPool(16);
   private static final CachingHttpClient httpClient;
@@ -31,75 +37,59 @@ public class GitHubActionsAnalyzer {
 
   public static void main(String[] args) throws Exception {
     try {
-      var totalDurationsByDay = new HashMap<LocalDate, Set<WorkflowData>>();
-
       for (var repo : getRepos()) {
         System.out.println(repo);
-        Map<LocalDate, Set<WorkflowData>> workflows = getWorkflowData(repo);
-        print(workflows);
-        System.out.println();
-
-        for (var entry : workflows.entrySet()) {
-          totalDurationsByDay
-              .computeIfAbsent(entry.getKey(), d -> new HashSet<>())
-              .addAll(entry.getValue());
-        }
+        getWorkflowData(repo);
       }
-
-      System.out.println("totals");
-      print(totalDurationsByDay);
-      System.out.println();
     } finally {
       httpClient.close();
       executor.shutdown();
     }
   }
 
-  private static Map<LocalDate, Set<WorkflowData>> getWorkflowData(String repo) throws Exception {
+  private static void getWorkflowData(String repo) throws Exception {
     var workflowRuns = getWorkflowRuns(repo);
 
-    var durationsByDay = new HashMap<LocalDate, Set<WorkflowData>>();
-
-    var futures = new ArrayList<Future<Optional<WorkflowData>>>();
     for (var workflowRun : workflowRuns) {
-      futures.add(
-          executor.submit(
-              () -> {
-                var jobs = getJobs(repo, workflowRun);
-                var totalUsage = Duration.ZERO;
-                Instant firstJobStartedAt = null;
-                for (var job : jobs) {
-                  if (job.started_at == null) {
-                    // job is queued and hasn't started
-                    continue;
-                  }
-                  if (job.completed_at != null) {
-                    if (firstJobStartedAt == null || job.started_at.isBefore(firstJobStartedAt)) {
-                      firstJobStartedAt = job.started_at;
-                    }
-                    var duration = Duration.between(job.started_at, job.completed_at);
-                    totalUsage = totalUsage.plus(duration);
-                  }
-                }
-                if (firstJobStartedAt == null) {
-                  return Optional.empty();
-                }
-                var date = LocalDate.ofInstant(workflowRun.created_at, TIME_ZONE);
-                return Optional.of(new WorkflowData(date, totalUsage));
-              }));
-    }
+      if (workflowRun.run_attempt > 1) {
+        // created_at is not reset on retries, so altDuration below is not useful
+        continue;
+      }
+      if (workflowRun.getDuration().compareTo(MIN_QUEUE_TIME) < 0
+          || workflowRun.getDuration().compareTo(WORKFLOW_TIMEOUT) > 0) {
+        continue;
+      }
+      var altDuration =
+          workflowRun.updated_at.toEpochMilli() - workflowRun.created_at.toEpochMilli();
+      altDuration /= 60000;
+      if (altDuration > 60 && altDuration < 360) { // 360+ probably means issue with test timeout
 
-    for (var future : futures) {
-      future
-          .get()
-          .ifPresent(
-              workflowData ->
-                  durationsByDay
-                      .computeIfAbsent(workflowData.date, d -> new HashSet<>())
-                      .add(workflowData));
-    }
+        List<JobsResponse.Job> jobs = getJobs(repo, workflowRun);
 
-    return durationsByDay;
+        Instant firstJobStartedAt =
+            jobs.stream().map(job -> job.started_at).min(Comparator.naturalOrder()).orElse(null);
+
+        Duration queueDuration;
+        if (firstJobStartedAt != null) {
+          queueDuration = Duration.between(workflowRun.created_at, firstJobStartedAt);
+        } else {
+          // there were no jobs run (e.g. all jobs were skipped), so essentially the whole run was
+          // queue time
+          queueDuration = workflowRun.getDuration();
+        }
+
+        if (queueDuration.compareTo(MIN_QUEUE_TIME) > 0) {
+          System.out.println(
+              workflowRun.created_at
+                  + " -- "
+                  + queueDuration.toMinutes()
+                  + " -- "
+                  + workflowRun.getDuration().toMinutes()
+                  + " -- "
+                  + workflowRun.html_url);
+        }
+      }
+    }
   }
 
   private static List<String> getRepos() throws Exception {
@@ -114,69 +104,114 @@ public class GitHubActionsAnalyzer {
   }
 
   private static List<RunsResponse.WorkflowRun> getWorkflowRuns(String repo) throws Exception {
-    var workflowRuns = new ArrayList<RunsResponse.WorkflowRun>();
-    int page = 1;
-    while (true) {
-      var runsResponse =
-          sendRepoRequest(repo, "/actions/runs?per_page=100&page=" + page, RunsResponse.class);
-      if (runsResponse.workflow_runs.isEmpty()) {
-        return workflowRuns;
+    return getWorkflowRuns(repo, START, END);
+  }
+
+  private static List<RunsResponse.WorkflowRun> getWorkflowRuns(
+      String repo, Instant start, Instant end) throws Exception {
+
+    var runsResponse = getRunsResponse(repo, start, end, 1);
+
+    if (runsResponse.total_count > 1000) {
+      // need to reduce time range since github limit search results to 1000 (even across
+      // pagination)
+
+      // splitting by days/hours/minutes/seconds (instead of into arbitray intervals)
+      // in order to improve cacheability of the results
+      var difference = Duration.between(start, end);
+      Duration interval;
+      if (difference.toHours() > 24) {
+        // split by days
+        interval = Duration.ofDays(1);
+      } else if (difference.toMinutes() > 60) {
+        // split by hours
+        interval = Duration.ofHours(1);
+      } else if (difference.toSeconds() > 60) {
+        // split by minutes
+        interval = Duration.ofMinutes(1);
+      } else {
+        // split by seconds
+        interval = Duration.ofSeconds(1);
       }
-      for (RunsResponse.WorkflowRun workflowRun : runsResponse.workflow_runs) {
-        if (LocalDate.ofInstant(workflowRun.created_at, TIME_ZONE).isBefore(START_DATE)) {
-          return workflowRuns;
-        }
-        workflowRuns.add(workflowRun);
+
+      var futures = new ArrayList<Future<List<RunsResponse.WorkflowRun>>>();
+
+      var from = start;
+      var to = start.plus(interval);
+      while (!to.isAfter(end)) {
+        Instant finalFrom = from;
+        Instant finalTo = to;
+        futures.add(executor.submit(() -> getWorkflowRuns(repo, finalFrom, finalTo)));
+        from = to;
+        to = to.plus(interval);
       }
-      page++;
+
+      var results = new ArrayList<RunsResponse.WorkflowRun>();
+      for (Future<List<RunsResponse.WorkflowRun>> future : futures) {
+        results.addAll(future.get());
+      }
+      return results;
     }
+
+    var results = new ArrayList<>(runsResponse.workflow_runs);
+    for (int page = 2; page <= Math.ceil(runsResponse.total_count / (double) PAGE_SIZE); page++) {
+      results.addAll(getRunsResponse(repo, start, end, page).workflow_runs);
+    }
+
+    if (results.size() != runsResponse.total_count) {
+      System.out.println("INCORRECT NUMBER OF RESULTS");
+      System.out.println(results.size());
+      System.out.println(runsResponse.total_count);
+      System.exit(1);
+    }
+
+    results.sort(Comparator.comparing(job -> job.created_at));
+    return results;
+  }
+
+  private static RunsResponse getRunsResponse(String repo, Instant from, Instant to, int page)
+      throws Exception {
+    return sendRepoRequest(
+        repo,
+        "/actions/runs?created="
+            + from
+            + ".."
+            + to
+            + "&event=pull_request&per_page="
+            + PAGE_SIZE
+            + "&page="
+            + page
+            + "&exclude_pull_requests=true",
+        RunsResponse.class);
   }
 
   private static List<JobsResponse.Job> getJobs(String repo, RunsResponse.WorkflowRun workflowRun)
       throws Exception {
-    var jobsResponse =
-        sendRepoRequest(
-            repo, "/actions/runs/" + workflowRun.id + "/jobs?per_page=1", JobsResponse.class);
-    var jobs = new ArrayList<JobsResponse.Job>();
-    int i = 1;
-    while (jobs.size() < jobsResponse.total_count) {
-      jobsResponse =
-          sendRepoRequest(
-              repo,
-              "/actions/runs/" + workflowRun.id + "/jobs?per_page=100&page=" + i,
-              JobsResponse.class);
-      jobs.addAll(jobsResponse.jobs);
-      i++;
+    var jobsResponse = getJobsResponse(repo, workflowRun.id, 1);
+
+    var results = new ArrayList<>(jobsResponse.jobs);
+    for (int page = 2; page <= Math.ceil(jobsResponse.total_count / (double) PAGE_SIZE); page++) {
+      results.addAll(getJobsResponse(repo, workflowRun.id, page).jobs);
     }
-    return jobs;
+
+    if (results.size() != jobsResponse.total_count) {
+      System.out.println("INCORRECT NUMBER OF RESULTS");
+      System.out.println(results.size());
+      System.out.println(jobsResponse.total_count);
+      System.exit(1);
+    }
+    return results;
+  }
+
+  private static JobsResponse getJobsResponse(String repo, long workflowRunId, int page)
+      throws Exception {
+    return sendRepoRequest(
+        repo,
+        "/actions/runs/" + workflowRunId + "/jobs?per_page=" + PAGE_SIZE + "&page=" + page,
+        JobsResponse.class);
   }
 
   private static <T> T sendRepoRequest(String repo, String path, Class<T> type) throws Exception {
     return httpClient.get("https://api.github.com/repos/open-telemetry/" + repo + path, type);
-  }
-
-  private static void print(Map<LocalDate, Set<WorkflowData>> durationsByDay) {
-    durationsByDay.entrySet().stream()
-        .sorted(Map.Entry.comparingByKey())
-        .filter(entry -> entry.getKey().getDayOfWeek().getValue() < 6) // week days
-        .forEach(
-            entry -> {
-              Set<WorkflowData> workflows = entry.getValue();
-              long totalUsage =
-                  workflows.stream().mapToLong(workflow -> workflow.totalUsage.toMinutes()).sum();
-              System.out.printf(
-                  "%s - %4d builds, total usage: %6.1f hours%n",
-                  entry.getKey(), workflows.size(), totalUsage / 60.0);
-            });
-  }
-
-  static class WorkflowData {
-    final LocalDate date;
-    final Duration totalUsage;
-
-    WorkflowData(LocalDate date, Duration totalUsage) {
-      this.date = date;
-      this.totalUsage = totalUsage;
-    }
   }
 }
--- src/main/java/com/github/trask/RunsResponse.java
+++ src/main/java/com/github/trask/RunsResponse.java
@@ -1,10 +1,12 @@
 package com.github.trask;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
 public class RunsResponse {
 
+  public int total_count;
   public List<WorkflowRun> workflow_runs;
 
   public static class WorkflowRun {
@@ -12,7 +14,14 @@ public class RunsResponse {
     // re-running an action does not reset the created_at
     // which makes it useless for calculating queue time / build time
     public Instant created_at;
+    public Instant updated_at;
 
-    public String url;
+    public int run_attempt;
+
+    public String html_url;
+
+    public Duration getDuration() {
+      return Duration.between(created_at, updated_at);
+    }
   }
 }
